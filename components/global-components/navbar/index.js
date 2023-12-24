9b148b84c382e9a945d42f7f99665c224cf6d5e3
Dariusz Stawik <dariusz.stawik@gmail.com>
2023-12-24 14:33:49 +0100

    Update navbar

--- components/global-components/navbar/index.js
+++ components/global-components/navbar/index.js
@@ -33,83 +33,92 @@ const Navbar = () => {
 
   return (
     <div className="bg-white border-b-2 border-b-gray-100 text-xl antialiased fixed w-screen h-28 flex justify-between items-center px-10 lg:px-16 py-4  top-0 left-0 z-50">
-      {/* <div className="h-28"> */}
-      <Logo closeMobileMenu={closeMobileMenu} />
-      <div className="pl-16 w-full flex justify-end items-center gap-6 h-28">
-        {/* <div className="relative"> */}
-        <NavigationMenu>
-          <NavigationMenuList>
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
             <ul
-              className={
-                isMobileMenuActive
-                  ? " list-none absolute z-40 w-screen top-full left-0 bg-gray-50 py-4 flex flex-col justify-center align-center items-center gap-10 lg:static lg:flex-row"
-                  : " list-none flex-col justify-center align-center items-center gap-10 lg:flex-row my-4 hidden lg:flex lg:justify-end ml-4"
-              }
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-96 text-sm overflow-scroll"
             >
-              {navbarData.map((nav) => {
-                const isActive = pathname === nav.path;
+              {navbarData.map((item) => (
+                <li>
+                  <a>{item.title}</a>
+                  {item.submenu && (
+                    <ul className="p-2">
+                      {item.submenu.map((subitem) => (
+                        <li>
+                          <a>{subitem.title}</a>
+                        </li>
+                      ))}
+                    </ul>
+                  )}
+                </li>
+              ))}
 
-                return (
-                  <NavigationMenuItem key={nav.id}>
-                    {nav.submenu ? (
-                      <>
-                        <NavigationMenuTrigger>
-                          {nav.title}
-                        </NavigationMenuTrigger>
-                        <NavigationMenuContent>
-                          <ul className="grid gap-3 p-6 md:w-[400px] lg:min-w-[900px] lg:grid-cols-[.75fr_1fr]">
-                            {nav.submenu.map((subnav) => {
-                              return (
-                                <li className="relative" key={subnav.id}>
-                                  {/* <NavigationMenuLink
-                                    className={navigationMenuTriggerStyle()}
-                                  > */}
-                                  <Link
-                                    href={subnav.path}
-                                    className={cn(
-                                      "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
-                                    )}
-                                  >
-                                    <div className="text-sm font-medium leading-none">
-                                      {subnav.title}
-                                    </div>
-                                  </Link>
-                                  {/* </NavigationMenuLink> */}
-                                </li>
-                              );
-                            })}
-                          </ul>
-                        </NavigationMenuContent>{" "}
-                      </>
-                    ) : (
-                      <>
-                        {/* <NavigationMenuLink
-                          className={navigationMenuTriggerStyle()}
-                        > */}
-                        <Link
-                          href={nav.path}
-                          className={cn(
-                            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
-                          )}
-                        >
-                          <div className="text-sm font-medium leading-none">
-                            {nav.title}
-                          </div>
-                        </Link>
-                        {/* </NavigationMenuLink> */}
-                      </>
-                    )}
-                  </NavigationMenuItem>
-                );
-              })}
+              {/* <li>
+                <a>Item 1</a>
+              </li>
+              <li>
+                <a>Parent</a>
+                <ul className="p-2">
+                  <li>
+                    <a>Submenu 1</a>
+                  </li>
+                  <li>
+                    <a>Submenu 2</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a>Item 3</a>
+              </li> */}
             </ul>
-          </NavigationMenuList>
-        </NavigationMenu>
+          </div>
+          <Logo />
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <a>Item 1</a>
+            </li>
+            <li>
+              <details>
+                <summary>Parent</summary>
+                <ul className="p-2">
+                  <li>
+                    <a>Submenu 1</a>
+                  </li>
+                  <li>
+                    <a>Submenu 2</a>
+                  </li>
+                </ul>
+              </details>
+            </li>
+            <li>
+              <a>Item 3</a>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <a className="btn">Button</a>
+        </div>
       </div>
-      <Hamburger
-        hasCloseIcon={isMobileMenuActive ? true : false}
-        toggleMobileMenu={toggleMobileMenu}
-      />
     </div>
   );
 };
